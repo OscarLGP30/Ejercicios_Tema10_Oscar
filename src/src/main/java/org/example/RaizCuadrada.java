2e19d49d328915100bc2ccad5a5700458dbc6df7
OscarLGP30 <olegaper@myuax.com>
2023-03-22 16:09:12 +0100

    Ejercicio de la Raiz cuadrada
    Took 21 minutes

--- src/src/main/java/org/example/RaizCuadrada.java
+++ src/src/main/java/org/example/RaizCuadrada.java
@@ -1,8 +1,30 @@
 package org.example;
 
+
+import java.util.Scanner;
+
 public class RaizCuadrada {
-    //Sea la función raizCuadrada() que calcula la raíz cuadrada del número que se pasa como argumento.
-    //Esta función lanza una excepción si su argumento es negativo.
-    //No se le pide que escriba esta función, se asume que ya existe.
-    //Escribir un algoritmo que calcule la raíz cuadrada de un número introducido por el usuario.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Pedir al usuario que introduzca un valor numérico
+        System.out.print("Introducir un valor: ");
+        double valor = scanner.nextDouble();
+
+        // Calcular la raíz cuadrada y mostrar el resultado o un mensaje de error
+        try {
+            double resultado = raizCuadrada(valor);
+            System.out.printf("√%.2f = %.2f", valor, resultado);
+        } catch (IllegalArgumentException e) {
+            System.out.println("La raíz cuadrada no está definida para un número negativo.");
+        }
+    }
+
+    public static double raizCuadrada(double x) {
+        if (x < 0) {
+            throw new IllegalArgumentException("El argumento debe ser no negativo");
+        }
+        return Math.sqrt(x);
+    }
 }
+
