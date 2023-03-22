74166d4cc638d87537918264924d1c0c6d45927d
OscarLGP30 <olegaper@myuax.com>
2023-03-22 15:45:55 +0100

    creo clase raiz cuadrada
    Took 6 minutes

---
+++ src/src/main/java/org/example/RaizCuadrada.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class RaizCuadrada {
+    //Sea la función raizCuadrada() que calcula la raíz cuadrada del número que se pasa como argumento.
+    //Esta función lanza una excepción si su argumento es negativo.
+    //No se le pide que escriba esta función, se asume que ya existe.
+    //Escribir un algoritmo que calcule la raíz cuadrada de un número introducido por el usuario.
+}
